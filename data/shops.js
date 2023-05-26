45272fbff98254a30c554fcb5a11ed3b44b088a5
Mariia Korol <marie.korol1991@gmail.com>
2023-05-26 12:11:40 +0300

    add shops

--- data/shops.js
+++ data/shops.js
@@ -1,4 +1,4 @@
 module.exports = () => {
-  const shops = ['Burgers', 'Sushi', 'Pizza', 'Salateria', 'Meat', 'Vegan', 'Pies', 'Desserts'];
+  const shops = ['Burgers', 'Sushi', 'Pizza', 'Salads', 'Meat', 'Vegan', 'Pies', 'Desserts'];
   return { shops };
 };
